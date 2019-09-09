c5558dc3331ab13f033db42740c7db2db7f57e96
Krystal Mitchell <10262399@my.uvu.edu>
2019-09-09 15:26:26 -0600

    test

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -30,6 +30,49 @@
       >
         <h2 class="headline font-weight-bold mb-3">This is what's next</h2>
 
+        <v-card
+      max-width="500"
+      class="mx-auto"
+    >
+      <v-toolbar
+        color="indigo"
+        dark
+      >
+        <v-app-bar-nav-icon></v-app-bar-nav-icon>
+  
+        <v-toolbar-title>Inbox</v-toolbar-title>
+
+        <div class="flex-grow-1"></div>
+  
+        <v-btn icon>
+          <v-icon>mdi-magnify</v-icon>
+        </v-btn>
+  
+        <v-btn icon>
+          <v-icon>mdi-dots-vertical</v-icon>
+        </v-btn>
+      </v-toolbar>
+      <v-list>
+        <v-list-item
+          v-for="item in items"
+          :key="item.title"
+          @click=""
+        >
+          <v-list-item-icon>
+            <v-icon v-if="item.icon" color="pink">mdi-star</v-icon>
+          </v-list-item-icon>
+  
+          <v-list-item-content>
+            <v-list-item-title v-text="item.title"></v-list-item-title>
+          </v-list-item-content>
+  
+          <v-list-item-avatar>
+            <v-img :src="item.avatar"></v-img>
+          </v-list-item-avatar>
+        </v-list-item>
+      </v-list>
+    </v-card>
+
       </v-flex>
 
       <v-flex
@@ -77,6 +120,17 @@
 //end of code
 export default {
   data: () => ({
+    //return {
+      items: [
+        { icon: true, title: 'Jason Oner', avatar: 'https://cdn.vuetifyjs.com/images/lists/1.jpg' },
+        { title: 'Travis Howard', avatar: 'https://cdn.vuetifyjs.com/images/lists/2.jpg' },
+        { title: 'Ali Connors', avatar: 'https://cdn.vuetifyjs.com/images/lists/3.jpg' },
+        { title: 'Cindy Baker', avatar: 'https://cdn.vuetifyjs.com/images/lists/4.jpg' },
+      ],
+    //}
+
+
+
     ecosystem: [
       {
         text: 'vuetify-loader',
