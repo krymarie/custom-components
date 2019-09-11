bb1582c7940706439fc22fa0f18c89442a107ae5
Krystal Mitchell <10262399@my.uvu.edu>
2019-09-11 15:35:59 -0600

    BTN

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -15,14 +15,6 @@
 
       <v-flex mb-4>
       <v-btn class="ma-2" outlined color="indigo">Outlined Button</v-btn>
-        <h1 class="display-2 font-weight-bold mb-3">
-          Krystal's Monster Project
-        </h1>
-        <p class="subheading font-weight-regular">
-          For help and collaboration with other Vuetify developers,
-          <br>please join our online
-          <a href="https://community.vuetifyjs.com" target="_blank">Discord Community</a>
-        </p>
       </v-flex>
 
       <v-flex
@@ -210,9 +202,6 @@
 <script>
 //end of code
 export default {
-
-
-
 
   data: () => ({
     drawer: true,
