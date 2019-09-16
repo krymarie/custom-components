851d7cddf131c2dc9f0748649b302d1fbfcf601f
Krystal Mitchell <10262399@my.uvu.edu>
2019-09-16 00:54:02 -0600

    turn method and ui

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -52,8 +52,8 @@
     <section class="row log">
         <div class="small-12 columns">
             <ul>
-                <li>
-
+                <li v-for="turn in turns">
+                  {{ turn.text }}
                 </li>
             </ul>
         </div>
@@ -71,6 +71,7 @@ export default {
     playerHealth: 100,
     monsterHealth: 100,
     gameIsRunning: false,
+    turns: []
   
      
   }),
@@ -82,7 +83,12 @@ export default {
       this.monsterHealth = 100;
     },
     attack: function() {
-      this.monsterHealth -= this.calculateDamage(3, 10);
+      var damage = this.calculateDamage(3, 10);
+      this.monsterHealth -= damage;
+      this.turns.unshift({
+        isPlayer: true,
+        text: 'Player hits Monster for ' + damage
+      });
       if(this.checkWin()) {
         return;
       }
@@ -113,8 +119,13 @@ export default {
       this.gameIsRunning = false;
     },
     monsterAttacks: function() {
-      this.playerHealth -= this.calculateDamage(5, 12);
+      var damage = this.calculateDamage(5, 12);
+      this.playerHealth -= damage;
       this.checkWin();
+      this.turns.unshift({
+        isPlayer: false;
+        text: 'Monster hits Player with ' + damage
+      })
     },
     
     calculateDamage: function(min, max) {
