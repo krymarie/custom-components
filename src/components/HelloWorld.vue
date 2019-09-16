1ddb2a98fabe9049ef171e7b85cb845afbca2ca1
Krystal Mitchell <10262399@my.uvu.edu>
2019-09-16 00:41:30 -0600

    mising ,

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -115,7 +115,7 @@ export default {
     monsterAttacks: function() {
       this.playerHealth -= this.calculateDamage(5, 12);
       this.checkWin();
-    }
+    },
     
     calculateDamage: function(min, max) {
       return Math.max(Math.floor(Math.random() * max) +1, min);
