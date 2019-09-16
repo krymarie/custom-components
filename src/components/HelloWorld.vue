d162b7fbbe89bfb75c0433a92df9b5609e0fb032
Krystal Mitchell <10262399@my.uvu.edu>
2019-09-15 22:15:57 -0600

    adjust healthbar per 100%

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -14,7 +14,11 @@
         <div class="small-6 columns">
             <h1 class="text-center">YOU</h1>
             <div class="healthbar">
-                <div class="healthbar text-center" style="background-color: green; margin: 0; color: white;">
+                <div 
+                class="healthbar text-center" 
+                style="background-color: green; 
+                margin: 0; color: white;
+                :style= "{width: playerHealth + '%'}">
                 {{playerHealth}}
                 </div>
             </div>
@@ -22,7 +26,11 @@
         <div class="small-6 columns">
             <h1 class="text-center">MONSTER</h1>
             <div class="healthbar">
-                <div class="healthbar text-center" style="background-color: green; margin: 0; color: white;">
+                <div 
+                class="healthbar text-center" 
+                style="background-color: green; 
+                margin: 0; color: white;
+                :style= "{width: monsterHealth + '%'}">
                 {{monsterHealth}}
                 </div>
             </div>
