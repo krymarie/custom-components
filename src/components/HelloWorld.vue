bea4e277888a780f9ead6ae337473c64e11ad6d8
Krystal Mitchell <10262399@my.uvu.edu>
2019-09-16 00:30:30 -0600

    special Attack

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -83,18 +83,21 @@ export default {
     },
     attack: function() {
       this.monsterHealth -= this.calculateDamage(3, 10);
-
       if(this.checkWin()) {
         return;
       }
 
       this.playerHealth -= this.calculateDamage(5, 12);
-
       this.checkWin();
     },
 
     specialAttack: function() {
+      this.monsterHealth -= this.calculateDamage(10, 20);
+      if(this.checkWin()) {
+        return;
+      }
 
+      this.monsterAttacks ();
     },
 
     heal: function() {
@@ -104,6 +107,10 @@ export default {
     giveUp: function() {
 
     },
+    monsterAttacks: function() {
+      this.playerHealth -= this.calculateDamage(5, 12);
+      this.checkWin();
+    }
     
     calculateDamage: function(min, max) {
       return Math.max(Math.floor(Math.random() * max) +1, min);
