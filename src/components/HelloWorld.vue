aa225012444069640fdd47acca0ba3e3a7a0ee25
Krystal Mitchell <10262399@my.uvu.edu>
2019-09-15 22:01:42 -0600

    mesing around with old logo

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -57,8 +57,6 @@
         <v-flex xs12>
         <v-img
           :src="require('../assets/logo.svg')"
-          class="my-3"
-          contain
           height="200"
         ></v-img>
       </v-flex>
