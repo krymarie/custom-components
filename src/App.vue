6dd2dcc15bfc61e95292a5a9ad05b7627221d0f3
Krystal Mitchell <10262399@my.uvu.edu>
2019-09-16 23:33:35 -0600

    v-card better footer fits design better

--- src/App.vue
+++ src/App.vue
@@ -22,6 +22,19 @@
       <div class="flex-grow-1 text-center"></div>
       <div>&copy; {{ new Date().getFullYear() }}</div>
     </v-footer>
+    <v-card height="150">
+      <v-footer
+        absolute
+        class="font-weight-medium"
+      >
+        <v-col
+          class="text-center"
+          cols="12"
+        >
+          {{ new Date().getFullYear() }} — <strong>Vuetify</strong>
+        </v-col>
+      </v-footer>
+    </v-card>
   </v-app>
 </template>
 
