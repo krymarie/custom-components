0da8fc0888f9cf95a73004ae4bf71d3a4c7c57d4
Krystal Mitchell <10262399@my.uvu.edu>
2019-09-16 20:44:58 -0600

    trying this modal thing one more time

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,9 +1,41 @@
-<template>
-  <v-container>
+  <div id="modal-app" class="uk-container uk-container-center uk-margin-top">
+  <button v-on="click: showModal = true" class="uk-button uk-button-primary">Show Modal</button>
+  
+  <modal show="{{@showModal}}">
+    <div class="modal-header">
+      <h3>
+        Hello Vue.JS
+      </h3>
+    </div>
+  </modal>
+</div>
+<template id="modal-template">
+<v-container>
     <v-layout
       text-center
       wrap
     >
+
+  <div class="modal" v-show="show" v-transition="modal">
+    <div class="modal-wrapper">
+      <div class="modal-container">
+        <content select=".modal-header">
+          <div class="modal-header">
+            <h3>
+              Hello World
+            </h3>
+          </div>
+        </content>
+        <div class="modal-body">
+          I said hello to world not you
+        </div>
+        <div class="modal-footer uk-clearfix">
+            <button v-on="click: show = false" class="uk-button uk-button-success uk-float-right">Ok, I hear!</button>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
 
         <v-flex xs12>
         <section class="ma-2" class="row">
