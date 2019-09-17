4879263d4c99e45d9003c9846518f8c37850cb83
Krystal Mitchell <10262399@my.uvu.edu>
2019-09-16 23:19:08 -0600

    yeah no

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -73,34 +73,60 @@
 
     </section
 
+    <v-footer
+      dark
+      padless
+    >
+      <v-card
+        class="flex"
+        flat
+        tile
+      >
+        <v-card-title class="teal">
+          <strong class="subheading">Get connected with us on social networks!</strong>
+  
+          <div class="flex-grow-1"></div>
+  
+          <v-btn
+            v-for="icon in icons"
+            :key="icon"
+            class="mx-4"
+            dark
+            icon
+          >
+            <v-icon size="24px">{{ icon }}</v-icon>
+          </v-btn>
+        </v-card-title>
+  
+        <v-card-actions class="grey darken-3 justify-center">
+          {{ new Date().getFullYear() }} — <strong>Vuetify</strong>
+        </v-card-actions>
+      </v-card>
+    </v-footer>
+
     </v-layout>
   </v-container>
 </template>
 
 <script>
-
-Vue.component('modal', {
-  template: '#modal-template',
-  props: {
-    show: {
-      type: Boolean,
-      required: true,
-      twoWay: true  
-    }
-  }
-});
-//possibly just use this here for the script for vue <script src="app.js">
-//end of code
 export default {
 
-  el: '#modal-app',
+  
+  el: '#app'
 
   data: () => ({
     showModal: false,
     playerHealth: 100,
     monsterHealth: 100,
     gameIsRunning: false,
-    turns: []
+    turns: [],
+    icons: [
+      'fab fa-facebook',
+      'fab fa-twitter',
+      'fab fa-google-plus',
+      'fab fa-linkedin',
+      'fab fa-instagram',
+    ],
   }),
   methods: {
     startGame: function() {
