53a41cf0f0a2008bb4056d88de373e961e508fc0
Krystal Mitchell <10262399@my.uvu.edu>
2019-10-07 22:46:57 -0600

    Not sure is all files are tracking but once this is linked it will work

--- src/App.vue
+++ src/App.vue
@@ -2,13 +2,13 @@
   <v-app>
     <v-app-bar app>
       <v-toolbar-title class="headline text-uppercase">
-        <span>MONSTER PROJECT</span>
+        <span>CUSTOM COMPONENTS</span>
         <span class="font-weight-light">KRYSTAL MITCHELL</span>
       </v-toolbar-title>
       <v-spacer></v-spacer>
       <v-btn
         text
-        href="https://github.com/krymarie/krystal-mitchell-monster-project/commits/master"
+        href="https://github.com/krymarie/krystal-mitchell-custom-components/commits/master"
         target="_blank"
       >
         <span class="mr-2">Latest Repo</span>
@@ -16,35 +16,42 @@
     </v-app-bar>
 
     <v-content text-center>
-      <HelloWorld/>
+      <div class="container">
+        <div class="row">
+            <div class="col-xs-12">
+                <app-user></app-user>
+            </div>
+        </div>
+    </div>
     </v-content>
-    
-    <v-card height="150">
-      <v-footer
-        absolute
-        class="font-weight-medium"
-      >
-        <v-col
-          class="text-center"
-          cols="12"
-        >
-          {{ new Date().getFullYear() }} — <strong>Monster Slayer</strong>
-        </v-col>
-      </v-footer>
-    </v-card>
-  </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld';
+//import HelloWorld from './components/HelloWorld';
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld,
-  },
-  data: () => ({
-    //
-  }),
-};
+// export default {
+//   name: 'App',
+//   components: {
+//     HelloWorld,
+//   },
+//   data: () => ({
+//     //
+//   }),
+// };
+
+import User from './components/User.vue';
+
+    export default {
+        components: {
+            appUser: User
+        }
+    }
 </script>
+
+<style>
+    div.component {
+        border: 1px solid black;
+        padding: 30px;
+    }
+</style>
+
--- src/main.js
+++ src/main.js
@@ -1,10 +1,18 @@
 import Vue from 'vue'
 import App from './App.vue'
-import vuetify from './plugins/vuetify';
+import vuetify from './plugins/vuetify';//not in maxes code
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false//not a clue what this was for
+
+export const eventBus = new Vue({
+  methods: {
+      changeAge(age) {
+          this.$emit('ageWasEdited', age);
+      }
+  }
+});
 
 new Vue({
   vuetify,
   render: h => h(App)
-}).$mount('#app')
+}).$mount('#app')//also not in maxes code
